7e092b74332cb9ffbd0df6e43d60476231a3912e
Aaron Miles <cool_jingle@hotmail.co.uk>
2015-11-11 14:45:48 +0000

    end of session margin auto changed to important

--- Memrise_CSS_Tweaks.user.js
+++ Memrise_CSS_Tweaks.user.js
@@ -4,7 +4,7 @@
 // @description    Personal preference css improvements
 // @match          http://www.memrise.com/course/*/garden/*
 // @match          http://www.memrise.com/garden/review/*
-// @version        0.0.2
+// @version        0.0.3
 // @updateURL      https://github.com/cooljingle/memrise-css-tweaks/raw/master/Memrise_CSS_Tweaks.user.js
 // @downloadURL    https://github.com/cooljingle/memrise-css-tweaks/raw/master/Memrise_CSS_Tweaks.user.js
 // @grant          none
@@ -25,4 +25,4 @@ sheet.addRule(".mem.garden", "margin:auto");
 //extend the length of the question row
 sheet.addRule(".question-row .qquestion", "width: 80%");
 //centre the end of session summary
-sheet.addRule(".garden-box.end_of_session", "margin: auto");
+sheet.addRule(".garden-box.end_of_session", "margin: auto !important");
