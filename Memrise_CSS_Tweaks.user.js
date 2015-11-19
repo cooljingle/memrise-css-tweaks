14a2776d7949cd87d94ea48f327320583c597d67
Aaron Miles <cool_jingle@hotmail.co.uk>
2015-11-19 14:37:26 +0000

    fix to weird end of session point rendering

--- Memrise_CSS_Tweaks.user.js
+++ Memrise_CSS_Tweaks.user.js
@@ -4,7 +4,7 @@
 // @description    Personal preference css improvements
 // @match          http://www.memrise.com/course/*/garden/*
 // @match          http://www.memrise.com/garden/review/*
-// @version        0.0.3
+// @version        0.0.4
 // @updateURL      https://github.com/cooljingle/memrise-css-tweaks/raw/master/Memrise_CSS_Tweaks.user.js
 // @downloadURL    https://github.com/cooljingle/memrise-css-tweaks/raw/master/Memrise_CSS_Tweaks.user.js
 // @grant          none
@@ -26,3 +26,5 @@ sheet.addRule(".mem.garden", "margin:auto");
 sheet.addRule(".question-row .qquestion", "width: 80%");
 //centre the end of session summary
 sheet.addRule(".garden-box.end_of_session", "margin: auto !important");
+//fix to weird end of session point rendering 
+sheet.addRule(".hovercard .points", "color: cadetblue; position: relative; background-image: none; display: inline");
